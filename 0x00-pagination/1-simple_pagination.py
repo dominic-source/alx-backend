7700d9e9a0ecaeb64ced7d9631ce4ad1c6dc6fb6
dominic-source <chinonsodomnic@gmail.com>
2024-01-26 17:55:08 +0100

    solved: add documentation to get_page method

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -28,6 +28,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """get the pages using its index"""
         assert isinstance(page, int)
         assert page > 0
         assert isinstance(page_size, int)
