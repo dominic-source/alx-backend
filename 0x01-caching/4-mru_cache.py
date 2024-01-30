d87d5bb7610e2865703761b1be30bfc0f60288ca
dominic-source <chinonsodomnic@gmail.com>
2024-01-30 19:36:12 +0100

    Practice: implement the MRU algorithm for caching

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+"""This module creates a basic cache class and
+   inherit from a base cache class
+"""
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """use most recently use approach to caching"""
+
+    def __init__(self):
+        """Initialization method"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Put item into the cache implementing the
+        MRU caching algorithm"""
+        if key and item:
+            self.cache_data.pop(key, None)
+            self.cache_data[key] = item
+            length = len(self.cache_data)
+            if length > BaseCaching.MAX_ITEMS:
+                key = list(self.cache_data)[-2]
+                del self.cache_data[key]
+                print("DISCARD:", key)
+
+    def get(self, key):
+        """get the data"""
+        if key:
+            value = self.cache_data.get(key, None)
+            if value:
+                del self.cache_data[key]
+                self.cache_data[key] = value
+            return value
+        return None
