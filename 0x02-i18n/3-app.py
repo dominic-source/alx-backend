df1e54f4313b04dd3b98596a5be8006d5ce22065
dominic-source <chinonsodomnic@gmail.com>
2024-02-07 08:23:19 +0100

    debug: documentation and file content v2

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -26,7 +26,8 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
-    """Get locale languages  for babels"""
+    """return language type for various users preference
+    """
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
