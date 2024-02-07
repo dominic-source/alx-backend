e27e617380e713a2a6d13df90d76bfee127fe559
dominic-source <chinonsodomnic@gmail.com>
2024-02-07 05:39:18 +0100

    adjust babel config - v5

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -20,19 +20,18 @@ app = Flask(__name__)
 # load configuration from class config object
 app.config.from_object(Config)
 
+# Instantiate Babel
+babel = Babel()
 
-# # Instantiate Babel
-# babel = Babel(app)
 
-
-# @babel.localeselector
+@babel.localeselector
 def get_locale():
     """Get locale languages"""
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 # Instantiate Babel
-babel = Babel(app, locale_selector=get_locale)
+babel.init_app(app)
 
 
 @app.route("/", strict_slashes=False, methods=["GET"])
